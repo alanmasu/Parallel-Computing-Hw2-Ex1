453ae30d2b16c1fcf05d2c4d6d003fb8c1e7ef20
alanmasu <alan.masutti@gmail.com>
2023-11-14 15:44:42 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,32 @@
+/*                      INTRODUCTION TO PARALLEL COMPUTING - Prof. VELLA 
+
+    Student: Alan Masutti - 226901 - alan.masutti@studenti.unitn.it
+
+    Assignment 2 - Exercise 1:
+      The first exercise consists of implementing a parallel matrix multiplication program using OpenMP.
+      The program must be able to handle efficiently both dense and sparse matrices of real numbers. The
+      tasks to be performed are:
+      - T1: Serial matrix multiplication:
+            Implement a serial matrix multiplication algorithm. You will need to define a matMul function
+            that takes two input matrices, A and B, and returns their product, C. Be sure to validate the
+            compatibility of the matrices for multiplication.
+      - T2: Parallel matrix multiplication:
+            Parallelize matrix multiplication using OpenMP. Create a matMulPar function that takes the
+            same inputs as the sequential version and parallelizes the algorithm. Experiment with different
+            OpenMP directives to improve performance. Explore different parallelization strategies to find
+            the most efficient implementation.
+      - T3: Performance analysis:
+            Evaluate the performance and scalability of your parallel implementation of matrix multiplication. Calculate speedup and efficiency gains for different matrix sizes and number of threads.
+            Consider for this task the time of the routine and compute the FLOPS as performance metrics. Identify potential bottlenecks or problems and propose optimizations to help
+            solve them.
+
+    Notes:
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+    return 0;
+}
