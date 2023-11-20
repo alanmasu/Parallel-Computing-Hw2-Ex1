6d645fe4cc36f9d396c4b0b714686a62a86e2dd3
alanmasu <alan.masutti@gmail.com>
2023-11-20 22:52:33 +0100

    Added Scripts - NOT ALL WORK FINE
      Script that work:
       - debugScript.sh
       - doScaling.pbs
       - doStrongScaling.sh [comment python part]
       - env-configuration.sh
       - testParallelism.sh
       - pytonCharts and all company of charts [PYTHON NOT WORK NOW]

---
+++ doWeakScaling.sh
@@ -0,0 +1,28 @@
+if test -f "a.out"; then
+    rm a.out
+fi
+echo "Doing Weak Scaling"
+for i in {1..5}
+do
+    echo "- $i -------------------"
+    echo "Serial Code"
+    gcc-7.5.0 ./src/main.c ./src/ex1.c -DCOMPILATION_NOTES="\"WEAK_SCALING;SERIAL_CODE\"" -DN=256 -DPRINT
+    ./a.out
+    echo "-------------------"
+    echo ""
+    for i in 1 2 4 8 16 32 64
+    do  
+        echo "Parallel Code with $i threads"
+        echo "-------------------"
+        dim=$((256*i))
+        gcc-7.5.0 ./src/main.c ./src/ex1.c -DCOMPILATION_NOTES="\"WEAK_SCALING;$i\"" -fopenmp -DN="$dim" -DPRINT
+        export OMP_NUM_THREADS=$i
+        ./a.out
+        echo "-------------------"
+        echo ""
+    done
+done
+
+source pytonWeakScalingCharts.sh
+echo "Done Weak Scaling"
+echo "-------------------"
